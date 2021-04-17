e1488ed1110b2869aac58bfe248f63e848700658
Tejinder Matharu <tejindermatharu@hotmail.com>
2021-04-17 18:11:10 +0100

    game section

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import {useHistory} from "react-router";
-import ScissorsLogo from "../../assets/images/Scissors.svg";
-import PaperLogo from "../../assets/images/Paper.svg";
-import RockLogo from "../../assets/images/Rock.svg";
+import ScissorsLogo from "../../assets/images/ScissorsCircle.svg";
+import PaperLogo from "../../assets/images/PaperCircle.svg";
+import RockLogo from "../../assets/images/RockCircle.svg";
 import ResultsLogo from "../../assets/images/results.svg";
 import BodySvg from "../../assets/images/Body.svg";
 import "./style.scss";
@@ -11,7 +11,12 @@ function Home() {
     const history = useHistory();
 
     const onGameItemClick = () => {
-        history.push("/game");
+        history.push({
+            pathname: "/game",
+            state: {
+                action: "Rock"
+            }
+        });
     };
 
     return (
@@ -23,10 +28,10 @@ function Home() {
                     <div onClick={onGameItemClick} className="rock__img">
                         <img src={RockLogo} alt="Rock Logo" />
                     </div>
-                    <div className="paper__img">
+                    <div onClick={onGameItemClick} className="paper__img">
                         <img src={PaperLogo} alt="Paper Logo" />
                     </div>
-                    <div className="scissors__img">
+                    <div onClick={onGameItemClick} className="scissors__img">
                         <img src={ScissorsLogo} alt="Scissors Logo" />
                     </div>
                 </div>
